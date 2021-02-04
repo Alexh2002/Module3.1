8ddd9b7ab4c1af8bd147cf1b7676332051de0ab0
Alexander <a13x.2002@gmail.com>
2021-02-04 13:38:48 -0700

    this is my module 3

---
+++ Module3.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Module3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            myClass.MyMethod<ChocolateCake>(new ChocolateCake(), 1);
+
+        }
+    }
+}
+
+public class MyClass
+{
+    public void MyMethod<T>(T myParameter, int myParameter2)
+    {
+        Console.WriteLine("my first parameter is: " + myParameter);
+        Console.WriteLine("my second parameter is: " + myParameter2);
+    }
+}
+
+public class ChocolateCake
+{
+    public string MyProperty { get; set; }
+}
